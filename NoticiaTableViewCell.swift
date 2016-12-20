784670f4da59d85ee847286a55edbbc41534c2ea
Árthur Aramaki Mota <kenaramaki@MacBook-Pro-de-Arthur.local>
2016-12-19 22:55:19 -0200

    Criação inicial do layout: TableView, ListagemViewController, DetalhesViewController, NoticiaTableViewCell

---
+++ NoticiaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoticiaTableViewCell.swift
+//  UOL_app
+//
+//  Created by Árthur Ken Aramaki Mota on 19/12/16.
+//  Copyright © 2016 Ken Aramaki. All rights reserved.
+//
+
+import UIKit
+
+class NoticiaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UOL_app/DetalhesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetalhesViewController.swift
+//  UOL_app
+//
+//  Created by Árthur Ken Aramaki Mota on 19/12/16.
+//  Copyright © 2016 Ken Aramaki. All rights reserved.
+//
+
+import UIKit
+
+class DetalhesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
